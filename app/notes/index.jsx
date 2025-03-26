fd7733364e016a773f07448ec3d48327e97f116d
mike mwanzi <mikeabere@gmail.com>
2025-03-26 21:18:19 +0300

    CSS of the Modal

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -10,6 +10,19 @@ const NoteScreen = () => {
    const [modalVisible, setModalVisible] = useState(false);
    const [newNote, setNewNote] = useState("");
 
+   //Add new note
+   const addNote = () =>{
+     if (newNote.trim() === "") return;
+
+     setNotes((prevNotes)=> [
+      ...prevNotes,
+      { id:Date.now.toString(), text:newNote }
+     ]);
+
+     setNewNote("");
+     setModalVisible(false);
+   }
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -96,6 +109,61 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 18,
     fontWeight: "bold",
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    justifyContent: "center",
+    fontWeight: "bold",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%",
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  modalButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cancelButton: {
+    backgroundColor: "#ccc",
+    justifyContent: "space-between",
+    borderRadius: 5,
+    padding: 10,
+    flex: 1,
+    marginRight: 10,
+    alignItems: "center",
+  },
+  cancelButtonText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  saveButton: {
+    backgroundColor: "#007bff",
+    borderRadius: 5,
+    padding: 10,
+    flex: 1,
+    alignItems: "center",
+  },
+  saveButtonText: {
+    fontSize: 16,
+    color: "#fff",
   },
 });
 
